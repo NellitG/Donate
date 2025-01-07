ff75925b42c0c594a10297b93898ebc957fb8959
NellitG <kamaunelson157@gmail.com>
2025-01-07 15:40:53 +0300

    message

--- database/migrations/2025_01_07_115742_create_goods_donations_table.php
+++ database/migrations/2025_01_07_115742_create_goods_donations_table.php
@@ -9,13 +9,17 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('goods_donations', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+    public function up()
+{
+    Schema::create('goods_donations', function (Blueprint $table) {
+        $table->id('goods_id');
+        $table->foreignId('donation_id')->constrained('donations');
+        $table->string('item_name');
+        $table->integer('quantity');
+        $table->timestamps();
+    });
+}
+
 
     /**
      * Reverse the migrations.
