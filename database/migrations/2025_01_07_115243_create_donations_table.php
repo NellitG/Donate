0fb283d9568a6ae42a93d375e36c37c482cb132a
NellitG <kamaunelson157@gmail.com>
2025-01-07 15:20:46 +0300

    message

--- database/migrations/2025_01_07_115243_create_donations_table.php
+++ database/migrations/2025_01_07_115243_create_donations_table.php
@@ -9,20 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up()
-    {
-        Schema::create('donations', function (Blueprint $table) {
-            $table->id('donation_id');
-            $table->foreignId('donor_id')->constrained('donors');
-            $table->foreignId('campaign_id')->nullable()->constrained('campaigns');
-            $table->decimal('amount', 10, 2);
-            $table->timestamp('donation_date')->useCurrent();
-            $table->enum('payment_method', ['Credit Card', 'PayPal', 'Bank Transfer']);
-            $table->enum('status', ['Pending', 'Completed', 'Failed'])->default('Pending');
-            $table->timestamps();
-        });
-    }
-    
+   public function up()
+{
+    Schema::create('donations', function (Blueprint $table) {
+        $table->id('donation_id');
+        $table->foreignId('donor_id')->constrained('donors');
+        $table->foreignId('campaign_id')->nullable()->constrained('campaigns');
+        $table->decimal('amount', 10, 2);
+        $table->timestamp('donation_date')->useCurrent();
+        $table->enum('payment_method', ['Credit Card', 'PayPal', 'Bank Transfer']);
+        $table->enum('status', ['Pending', 'Completed', 'Failed'])->default('Pending');
+        $table->timestamps();
+    });
+}
+
     /**
      * Reverse the migrations.
      */
